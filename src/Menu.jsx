65af2f694bfeaf7cdf2b641ad6636570a8e767b3
Eric <erictang@umass.edu>
2019-03-25 16:27:34 -0400

    first menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -6,19 +6,40 @@ const state = [
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-class MyComponent extends React.Component {
-  constructor() {
-    super();
-  }
-
+class Menu extends React.Component {
   render() {
     return (
       <div>
-        <h1>My View 01</h1>
+        <a href="game.html"><h2>New Game</h2></a>
+        <a href=""><h2>Load Game</h2></a>
+        <a href=""><h2>Options</h2></a>
+      </div>
+    );
+  }
+}
+
+class Title extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>S a m p l e T e x t</h1>
+      </div>
+    );
+  }
+}
+
+class MenuScreen extends React.Component {
+  render() {
+    return (
+      <div>
+        <Title />
+        <hr />
+        <Menu />
+        <hr />
       </div>
     );
   }
 }
 
 // This renders the JSX component inside the content node:
-ReactDOM.render(<MyComponent />, contentNode);
+ReactDOM.render(<MenuScreen />, contentNode);
---
+++ static/Menu.js
@@ -0,0 +1,122 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// This is a place holder for the initial application state.
+var state = [];
+
+// This grabs the DOM element to be used to mount React components.
+var contentNode = document.getElementById("contents");
+
+var Menu = function (_React$Component) {
+  _inherits(Menu, _React$Component);
+
+  function Menu() {
+    _classCallCheck(this, Menu);
+
+    return _possibleConstructorReturn(this, (Menu.__proto__ || Object.getPrototypeOf(Menu)).apply(this, arguments));
+  }
+
+  _createClass(Menu, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "a",
+          { href: "game.html" },
+          React.createElement(
+            "h2",
+            null,
+            "New Game"
+          )
+        ),
+        React.createElement(
+          "a",
+          { href: "" },
+          React.createElement(
+            "h2",
+            null,
+            "Load Game"
+          )
+        ),
+        React.createElement(
+          "a",
+          { href: "" },
+          React.createElement(
+            "h2",
+            null,
+            "Options"
+          )
+        )
+      );
+    }
+  }]);
+
+  return Menu;
+}(React.Component);
+
+var Title = function (_React$Component2) {
+  _inherits(Title, _React$Component2);
+
+  function Title() {
+    _classCallCheck(this, Title);
+
+    return _possibleConstructorReturn(this, (Title.__proto__ || Object.getPrototypeOf(Title)).apply(this, arguments));
+  }
+
+  _createClass(Title, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "h1",
+          null,
+          "S a m p l e T e x t"
+        )
+      );
+    }
+  }]);
+
+  return Title;
+}(React.Component);
+
+var MenuScreen = function (_React$Component3) {
+  _inherits(MenuScreen, _React$Component3);
+
+  function MenuScreen() {
+    _classCallCheck(this, MenuScreen);
+
+    return _possibleConstructorReturn(this, (MenuScreen.__proto__ || Object.getPrototypeOf(MenuScreen)).apply(this, arguments));
+  }
+
+  _createClass(MenuScreen, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(Title, null),
+        React.createElement("hr", null),
+        React.createElement(Menu, null),
+        React.createElement("hr", null)
+      );
+    }
+  }]);
+
+  return MenuScreen;
+}(React.Component);
+
+// This renders the JSX component inside the content node:
+
+
+ReactDOM.render(React.createElement(MenuScreen, null), contentNode);
