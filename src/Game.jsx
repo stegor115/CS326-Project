e81319742b5507e001722775647eb9151b68d52c
Stephen Gordon <stegor115@gmail.com>
2019-03-26 16:53:10 -0400

    Demo working

--- src/Game.jsx
+++ src/Game.jsx
@@ -9,19 +9,44 @@ var contentNode = document.getElementById("contents");
 class MyComponent extends React.Component {
   constructor() {
     super();
+    this.state = { game: true };
+  }
+
+  readCommand(bool){
+    console.log(document.getElementById("command").value);
+    if(document.getElementById("command").value === "attack"){
+      this.setState({ game: false });
+      document.getElementById("command").value = ""
+      console.log("Game: " + this.state.game);
+    }
   }
 
   render() {
-    return (
-      <div>
-        <img src="img/sample.jpg" alt="Sample image2" width="960" height="400"></img>
-        <div class="solid">
-          Terminal
-          <br></br>
-          <input type="text"></input>
+    if(this.state.game === true){
+      return (
+        <div>
+          <img src="img/sample.jpg" alt="Sample image" width="960" height="400"></img>
+            You are faced with a mighty foe in a suit! What do you do?
+            <br></br>
+            <input type="text" id="command"></input>
+            <br></br>
+            <button type="button" onClick ={ () => this.readCommand()}>Enter Command</button>
+            <a href="end.html"><button type="button">Quit</button></a>
         </div>
-      </div>
-    );
+      );
+    }else{
+      return (
+      <div>
+          <img src="img/boom.jpg" alt="Bang" width="960" height="400"></img>
+            You have defeated the mighty foe!
+            <br></br>
+            <input type="text" id="command"></input>
+            <br></br>
+            <button type="button" onClick ={ () => this.readCommand()}>Enter Command</button>
+            <a href="end.html"><button type="button">Quit</button></a>
+        </div>
+      );
+    }
   }
 }
 
