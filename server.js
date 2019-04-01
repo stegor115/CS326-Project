afbed9338ad8e6e945ab09607b249b1563729df1
Eric <erictang@umass.edu>
2019-04-01 16:43:05 -0400

    server.js modifications

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
 app.use(express.static('static'));
+app.use(bodyParser.json());
 
 app.listen(3000, function () {
     console.log('App started on port 3000');
