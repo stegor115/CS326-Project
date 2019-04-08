b84496fc2c1afe3f1984a295814997979464398f
Eric <erictang@umass.edu>
2019-04-08 16:29:02 -0400

    server.js set up for db

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ app.use(bodyParser.json());
 app.listen(3000, function () {
     console.log('App started on port 3000');
 });
+
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+
+MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
+  db = connection.db('commanddb');
+  app.listen(3000, () => {
+    console.log('App started on port 3000');
+  });
+}).catch(error => {
+  console.log('ERROR:', error);
+});
